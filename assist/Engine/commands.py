2747e623ebc0332deef4ea84297a75c81dc34a5a
Rathore-Rajpal <rathorerajpal18112003@gmail.com>
2025-02-23 11:59:54 +0530

    Speech recognigition part

--- assist/Engine/commands.py
+++ assist/Engine/commands.py
@@ -1,15 +1,36 @@
 import pyttsx3
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[0].id)
     engine.setProperty('rate', 174) 
-    print(voices)
     engine.say(text)
     engine.runAndWait()
 
-speak("India is my counrty and all indian are my brohters and sisters")
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listning")
+        r.pause_threshold= 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+    
+    try:
+        print("recgnozing")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User Said: {query}")
+    
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+text = takecommand()
+
+speak(text)
 
  
 
