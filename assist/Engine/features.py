5b2b41f091a4c7a387261f422868943f7bbeb4e0
Rathore-Rajpal <rathorerajpal18112003@gmail.com>
2025-02-23 10:10:45 +0530

    siriwave and start audio added

---
+++ assist/Engine/features.py
@@ -0,0 +1,6 @@
+import playsound as playsound
+
+#playing assistant sound function.
+def playAssistantSound():
+    mis_dir = "C:\\VirtualMouseProject\\assist\\www\\assets\\audio\\start_sound.mp3"
+    playsound(mis_dir)
--- assist/www/main.js
+++ assist/www/main.js
@@ -9,4 +9,29 @@ $(document).ready(function () {
             effect: "bounceOut",
         },
     })
+
+    //siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 880,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autoStart: true
+      });
+
+      //siri message animation
+      $('.siri-message').textillate({
+        loop: true,
+        sync: requestAnimationFrame,
+        in:{
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out:{
+            effect: "fadeOutUp",
+            sync: true,
+        },
+    })
 });
